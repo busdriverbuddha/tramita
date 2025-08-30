5e3becbd5c73fe9d0897dc1d9f746e79b0865d19
Guilherme Gama <guilhermegama@gmail.com>
2025-08-30 12:27:38 -0300

    stashing

--- dumpcode.py
+++ dumpcode.py
@@ -2,5 +2,22 @@ from pathlib import Path
 
 SRC_DIR = Path("./src/tramita")
 
+
+def tag_file(file_path: Path, tag_text: str):
+    lines: list[str] = file_path.read_text().splitlines()
+    while lines and (lines[0].strip() == "" or lines[0].startswith("#")):
+        lines.pop(0)
+
+    header_lines = [
+        f"# {tag_text}",
+        ""
+    ]
+
+    lines = header_lines + lines
+    tagged_text = "\n".join(lines)
+    file_path.write_text(tagged_text)
+
+
 for filename in SRC_DIR.rglob("*.py"):
-    print(filename.relative_to(SRC_DIR))
+    rel_path = filename.relative_to(SRC_DIR)
+    tag_file(filename, str(rel_path))
